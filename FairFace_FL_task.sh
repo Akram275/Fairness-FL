03157d72d33a6bc065bb0f39bd2d12a84bc70047
Akram <32540529+Akram275@users.noreply.github.com>
2024-03-29 09:02:51 +0100

    Script to submit the FairFace FL training job to computecanada graham cluster
    run : sbatch FairFace_FL_task.sh on the graham cluster

---
+++ FairFace_FL_task.sh
@@ -0,0 +1,16 @@
+  GNU nano 4.6                          FairFace_FL_task.sh                                     
+#!/bin/bash
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=20   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=100000M        # memory per node
+#SBATCH --time=0-2:30      # time (DD-HH:MM)
+#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+
+source tensorflow/bin/activate 
+module load scipy-stack
+pip install tensorflow==2.9.0
+pip install sklearn
+pip install shap
+module load cuda cudnn 
+export TF_GPU_ALLOCATOR=cuda_malloc_async
+python n_clients_improoved_FL.py
